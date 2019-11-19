57e937da6f97c8e8f39e340708343c3d859fae05
Changyu_Liu <liuchangyu1111@gmail.com>
2019-11-19 11:31:45 +0800

    Update dcgan_64x64.py

--- dcgan_64x64.py
+++ dcgan_64x64.py
@@ -66,7 +66,8 @@ def train():
   ################################################
   dataset = dset.ImageFolder(root=opt.dataroot,
                              transform=transforms.Compose([
-                               transforms.RandomResizedCrop(64),
+                               transforms.CenterCrop(64),
+                               # transforms.RandomResizedCrop(64),
                                transforms.ToTensor(),
                                transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5)),
                              ]))
